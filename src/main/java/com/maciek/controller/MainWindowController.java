f37b8e9107e7a51838ec4abada9bc40db57b6ca4
Maciej Bombik <maciej.bombik.programista@gmail.com>
2020-05-07 11:37:45 +0200

    java fx i maven setup OK

---
+++ src/main/java/com/maciek/controller/MainWindowController.java
@@ -0,0 +1,20 @@
+package com.maciek.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+
+
+    @FXML
+    private Label etykietaMiasta;
+
+    @FXML
+    private TextField wyborMiasta;
+
+    @FXML
+    void button1Action() {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Aplikacja.Pogodowa1 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.web;
+    requires javafx.graphics;
+
+    opens com.maciek;
+    opens com.maciek.controller;
+    opens com.maciek.view;
+}

f3732290bd21de0c78a26e82f2d66c57c4fec9f3
Maciej Bombik <maciej.bombik.programista@gmail.com>
2020-05-24 16:14:27 +0200

    stworzono artefakt

---
+++ out/artifacts/Aplikacja_Pogodowa_1_jar/run.bat
@@ -0,0 +1,1 @@
+java --module-path "C:\Users\matrix\Desktop\Java FX lib\openjfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2" --add-modules=javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar AplikacjaPogodowa.jar
